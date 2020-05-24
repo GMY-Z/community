1426a1c92056bd281944cb3619eb288008a84539
Guanmuyang <1321879427@qq.com>
2020-05-24 21:23:49 +0800

    navigation.html

--- src/main/java/life/manong/community/controller/IndexController.java
+++ src/main/java/life/manong/community/controller/IndexController.java
@@ -47,7 +47,7 @@ public class IndexController {
     @GetMapping("/")
     public String index(HttpServletRequest request, Model model,
                         @RequestParam(name = "page",defaultValue = "1")Integer page,
-                        @RequestParam(name = "size",defaultValue = "2") Integer size) {
+                        @RequestParam(name = "size",defaultValue = "5") Integer size) {
 
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
